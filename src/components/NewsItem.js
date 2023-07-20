4ec4424276a459fba514ee18b14d5a36fc84585b
Yoshita Shaw <109474278+yoshitashaw@users.noreply.github.com>
2023-07-20 16:19:10 +0000

    Add files via upload

---
+++ src/components/NewsItem.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+export class NewsItem extends Component {
+    render() {
+        let {title, description, imageUrl, url} = this.props;
+        return(
+            <div className= "my-3">
+                <div className="card" style={{width: "18rem"}}>
+                <img src={imageUrl? imageUrl: "https://img.freepik.com/free-photo/purple-osteospermum-daisy-flower_1373-16.jpg?size=626&ext=jpg"} /> 
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a rel="noreferrer" href={url} target= "_blank" className="btn btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NewsItem
