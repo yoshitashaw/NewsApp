c9c98c5d406cd9dd3377d0a0c6b077e7efc03531
Yoshita Shaw <109474278+yoshitashaw@users.noreply.github.com>
2023-07-21 17:26:05 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import logo from './logo.png';
+import {Link} from "react-router-dom";
 
 export default class Navbar extends Component {
   render() {
@@ -8,23 +9,26 @@ export default class Navbar extends Component {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <img src={logo} alt="logo" width="80" height="60" className="d-inline-block align-text-top"/>
-            <a className="navbar-brand" href="/">News App</a>
+            <Link className="navbar-brand" to="/">News App</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="/about">About</a>
-                </li>
+                  <li className="nav-item"> <Link className="nav-link active" aria-current="page" to="/">Home</Link> </li>
+                  {/* <li className="nav-item"> <Link className="nav-link" to="/about">About</Link> </li> */}
+                  <li className="nav-item"> <Link className="nav-link" to="/business">Business</Link> </li>
+                  <li className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link> </li>
+                  {/* <li className="nav-item"> <Link className="nav-link" to="/general">General</Link> </li> */}
+                  <li className="nav-item"> <Link className="nav-link" to="/health">Health</Link> </li>
+                  <li className="nav-item"> <Link className="nav-link" to="/science">Science</Link> </li>
+                  <li className="nav-item"> <Link className="nav-link" to="/sports">Sports</Link> </li>
+                  <li className="nav-item"> <Link className="nav-link" to="/technology">Technology</Link> </li>
               </ul>
-              <form className="d-flex" role="search">
+              {/* SearchBAR <form className="d-flex" role="search">
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                 <button className="btn btn-outline-warning" type="submit">Search</button>
-              </form>
+              </form> */}
             </div>
           </div>
         </nav>
